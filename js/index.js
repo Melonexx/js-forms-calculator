b2b430c0a2d87c44f9c4382a704400ee0598fcd6
Eileen Ilsemann <eileenilsemanni@gmail.com>
2024-01-05 13:13:21 +0100

    add solutuion

--- js/index.js
+++ js/index.js
@@ -22,11 +22,32 @@ function divide(a, b) {
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
+  // Holen Sie die Werte der Eingabefelder
+  const numberA = parseFloat(document.querySelector('[name="numberA"]').value);
+  const numberB = parseFloat(document.querySelector('[name="numberB"]').value);
+
+  // Holen Sie den ausgewählten Operator
+  const operatorInput = document.querySelector(
+    'input[name="operator"]:checked'
+  );
+  const operator = operatorInput ? operatorInput.value : null;
+
   let result;
 
-  // --v-- write your code here --v--
+  // Überprüfen Sie, welcher Operator ausgewählt wurde
+  if (operator === "addition") {
+    result = add(numberA, numberB);
+  } else if (operator === "subtraction") {
+    result = subtract(numberA, numberB);
+  } else if (operator === "multiplication") {
+    result = multiply(numberA, numberB);
+  } else if (operator === "division") {
+    // Überprüfen Sie, ob der Divisor nicht 0 ist, um eine Division durch 0 zu vermeiden
+    result = numberB !== 0 ? divide(numberA, numberB) : "Cannot divide by zero";
+  } else {
+    result = "Invalid operator";
+  }
 
-  // --^-- write your code here --^--
-
+  // Ergebnis zuweisen
   resultOutput.textContent = result;
 });
